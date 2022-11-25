a44cf920578ca479b845257127b82c4f9265ef7f
RodolphoBuettel <Rodolpho05buettel@gmail.com>
2022-11-25 14:43:41 -0300

    checkou/payment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import GlobalStyle from "./GlobalStyle.js";
 import { UserProvider } from "./contexts/contextApi.js";
 import Products from "./pages/products/products.js";
-
+import Checkout from "./pages/checkout/Checkout.js";
+import Payment from "./pages/payment/payment.js";
 
 export default function App() {
   return (
@@ -11,6 +12,8 @@ export default function App() {
        <GlobalStyle/>
            <Routes>
            <Route path="/products" element = {<Products/>}/>
+           <Route path="/checkout" element = {<Checkout/>}/>
+           <Route path="/payment" element = {<Payment/>}/>
            </Routes>
    </BrowserRouter>
    </UserProvider>
--- src/contexts/contextApi.js
+++ src/contexts/contextApi.js
@@ -1,12 +1,24 @@
-import { createContext } from "react";
+import { createContext, useState} from "react";
 
 const UserContext = createContext();
 
 function UserProvider({ children }) {
-
+    
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [country, setCountry] = useState("");
+    const [city, setCity] = useState("");
+    const [address, setAddress] = useState("");
+    const [numberCard, setNumberCard] = useState("");
+    const [nameCard, setNameCard] = useState("");
+    const [validity, setValidity] = useState("");
+    const [securityCode, setSecurityCode] = useState("");
     
     return (
-        <UserContext.Provider value={children}>
+        <UserContext.Provider value={{email, setEmail, name, setName, lastName, setLastName, country, setCountry,
+         city, setCity, address, setAddress, numberCard, setNumberCard, nameCard, setNameCard, validity, setValidity, 
+         securityCode, setSecurityCode}}>
             {children}
         </UserContext.Provider>
     );
